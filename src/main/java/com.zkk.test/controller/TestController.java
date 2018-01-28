ee95da75ef30cd3254a5e603e3a26893f574d257
xiaoaidelala <zkkxian@163.com>
2018-01-28 17:49:42 +0800

    update

---
+++ src/main/java/com.zkk.test/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.zkk.test.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zkk on 2018/1/28 17:45 .
+ */
+@Controller
+public class TestController {
+
+}
